df5cb5acca9bf1b45cc5221fbe6f9cee731260ed
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2021-02-05 13:34:48 -0300

    Spring initializr.

---
+++ src/main/java/com/zanatta/desafioconquer/DesafioConquerApplication.java
@@ -0,0 +1,13 @@
+package com.zanatta.desafioconquer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioConquerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioConquerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zanatta/desafioconquer/DesafioConquerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanatta.desafioconquer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioConquerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
